022231f4417b96b6e2d8e1371d876884dc5b712e
Patthew <Patthew@DESKTOP-7UAATA5>
2019-04-09 18:46:36 +0800

    v2.1
    improved, after "exposed" to Pong game code

--- src/uart_try/PlayerState.java
+++ src/uart_try/PlayerState.java
@@ -11,10 +11,10 @@ public class PlayerState {
     //fields
     //int pNo = 0;
     int conType = 0;
-    int lasX = 0;   //left analog stick
-    int lasY = 0;
-    int rasX = 0;   //right analog stick
-    int rasY = 0;
+    int lasX = 128;   //left analog stick
+    int lasY = 128;     //128  <-- neutral position (0 to 255)
+    int rasX = 128;   //right analog stick
+    int rasY = 128;
     boolean fbA = false;
     boolean fbB = false;
     boolean fbX = false;
@@ -34,25 +34,33 @@ public class PlayerState {
 
     
     //methods
-    public int getBit(byte input,int position){ //LSB is posn 0
+    public static int readAsUnsignedByte(byte input){
+        if(input<0){
+            return input + 256;
+        } else {
+            return input;
+        }
+    }
+    
+    public static int getBit(byte input,int position){ //LSB is posn 0
        return (input >> position) & 1;
     }
     
-    boolean boolFrBit(int input){   //1/0 to true/false  [logic similar to C]
+    static boolean boolFrBit(int input){   //1/0 to true/false  [logic similar to C]
         return input!=0;
     }
     
-    boolean boolFrByte(byte input,int position){   //1/0 to true/false  [logic similar to C]
+    static boolean boolFrByte(byte input,int position){   //1/0 to true/false  [logic similar to C]
         return boolFrBit(getBit(input,position));
     }
     
     void type1(byte[] byteBuffer){
         //pNo = byteBuffer[2] >> 4;
         conType 	= byteBuffer[2] & 0b00001111;
-        lasX 		= byteBuffer[3];   //left analog stick
-        lasY 		= byteBuffer[4];
-        rasX 		= byteBuffer[5];   //right analog stick
-        rasY 		= byteBuffer[6];
+        lasX 		= readAsUnsignedByte(byteBuffer[3]);   //left analog stick
+        lasY 		= readAsUnsignedByte(byteBuffer[4]);        //value from 0 to 255.
+        rasX 		= readAsUnsignedByte(byteBuffer[5]);   //right analog stick
+        rasY 		= readAsUnsignedByte(byteBuffer[6]);
         fbA 		= boolFrByte(byteBuffer[7],7);
         fbB 		= boolFrByte(byteBuffer[7],6);
         fbX 		= boolFrByte(byteBuffer[7],5);
@@ -77,19 +85,19 @@ public class PlayerState {
         conType 	= byteBuffer[2] & 0b00001111;
         switch (reqByteNum){
             case 3:
-                lasX 		= byteBuffer[3];   //left analog stick
+                lasX 		= readAsUnsignedByte(byteBuffer[3]);   //left analog stick
                 break;
                 
             case 4:
-                lasY 		= byteBuffer[3];
+                lasY 		= readAsUnsignedByte(byteBuffer[3]);
                 break;
                 
             case 5:
-                rasX 		= byteBuffer[3];   //right analog stick
+                rasX 		= readAsUnsignedByte(byteBuffer[3]);   //right analog stick
                 break;
                 
             case 6:
-                rasY 		= byteBuffer[3];
+                rasY 		= readAsUnsignedByte(byteBuffer[3]);
                 break;
                 
             case 7:
@@ -125,6 +133,154 @@ public class PlayerState {
                 break;
         }
     }
+    
+    
+    boolean readButton(CM button){
+        switch(button){
+            case A_FACE_BUTTON:
+                return fbA;
+                
+            case B_FACE_BUTTON:
+                return fbB;
+                
+            case X_FACE_BUTTON:
+                return fbX;
+                
+            case Y_FACE_BUTTON:
+                return fbY;
+                
+            case UP_DPAD:
+                return dpU;
+                
+            case DOWN_DPAD:
+                return dpD;
+                
+            case LEFT_DPAD:
+                return dpL;
+                
+            case RIGHT_DPAD:
+                return dpR;
+                
+            case START_BUTTON:
+                return btnStart;
+                
+            case SELECT_BUTTON:
+                return btnSelect;
+                
+            case LEFT_STICK_BUTTON:
+                return stickL;
+                
+            case RIGHT_STICK_BUTTON:
+                return stickR;
+                
+            case LEFT_SHOULDER:
+                return shL;
+                
+            case RIGHT_SHOULDER:
+                return shR;
+                
+            case LEFT_TRIGGER:
+                return trigL;
+                
+            case RIGHT_TRIGGER:
+                return trigR;
+                
+            default:
+                System.err.println("Oi, ["+button+"] is not a Button. It's a Stick Direction!");
+                return false;       
+        }
+    }
+    
+    
+    int readAnalogAxis(CM axis){
+        switch(axis){
+            case LEFT_ANALOG_STICK_X:
+                return lasX;
+                
+            case LEFT_ANALOG_STICK_Y:
+                return lasY;
+                
+            case RIGHT_ANALOG_STICK_X:
+                return rasX;
+                
+            case RIGHT_ANALOG_STICK_Y:
+                return rasY;
+                
+            default:
+                System.err.println("Hey, ["+axis+"] is not an Analog Axis. It is just a simple button!");
+                return 127;
+        }
+    }
+    
+    static CM readStickChange(PlayerState oldP, PlayerState newP){
+        //System.out.println("readStickChange:");
+        //System.out.println("oldP = " + oldP);
+        //System.out.println("newP = " + newP);
+        //System.out.println("--------------------------");
+        //System.out.println("--------------------------");
+        if(oldP.lasX != newP.lasX){
+            return CM.LEFT_ANALOG_STICK_X;
+        } else if(oldP.lasY != newP.lasY){
+            return CM.LEFT_ANALOG_STICK_Y;
+        } else if(oldP.rasX != newP.rasX){
+            return CM.RIGHT_ANALOG_STICK_X;
+        } else if(oldP.rasY != newP.rasY){
+            return CM.RIGHT_ANALOG_STICK_Y;
+        } else {
+            return null;
+        }
+    }
+    
+    static CM readBtnPressed(PlayerState oldP, PlayerState newP){
+        //System.out.println("readBtnPressed:");
+        //System.out.println("oldP = " + oldP);
+        //System.out.println("newP = " + newP);
+        //System.out.println("--------------------------");
+        //System.out.println("--------------------------");
+        for (CM key : CM.values()) {
+            switch(key){
+                case  LEFT_ANALOG_STICK_X:
+                case  LEFT_ANALOG_STICK_Y:
+                case  RIGHT_ANALOG_STICK_X:
+                case  RIGHT_ANALOG_STICK_Y:
+                    break;  //break switch (NOT for each)
+                default:
+                    if(newP.readButton(key) && !oldP.readButton(key)){
+                        return key;
+                    }
+                    break;
+            }
+        }
+        return null;
+    }
+    
+    
+    public PlayerState copy(){
+        PlayerState out = new PlayerState();
+        out.conType             = this.conType; 	
+        out.lasX 		= this.lasX; 		   //left analog stick
+        out.lasY 		= this.lasY; 		        //value from 0 to 255.
+        out.rasX 		= this.rasX; 		   //right analog stick
+        out.rasY 		= this.rasY; 		
+        out.fbA 		= this.fbA; 		
+        out.fbB 		= this.fbB; 		
+        out.fbX 		= this.fbX; 		
+        out.fbY 		= this.fbY; 		
+        out.dpD 		= this.dpD; 		
+        out.dpR 		= this.dpR; 		
+        out.dpL 		= this.dpL; 		
+        out.dpU 		= this.dpU; 		
+        out.btnStart            = this.btnStart; 	
+        out.btnSelect           = this.btnSelect; 
+        out.stickL 		= this.stickL; 	
+        out.stickR 		= this.stickR; 	
+        out.shL 		= this.shL; 		//shoulder button
+        out.shR 		= this.shR; 		
+        out.trigL 		= this.trigL; 	  //trigger button
+        out.trigR 		= this.trigR;
+        /////
+        return out;
+    } 
     
     
     // <editor-fold defaultstate="collapsed" desc="toString shortcut">
